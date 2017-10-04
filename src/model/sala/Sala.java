e568705dd15c14028be6c3986bd2e1b0c9f709c8
Kadu <carlosefrfloresta@gmail.com>
2017-10-04 20:04:59 -0300

    implementacao, classe sala

--- src/model/sala/Sala.java
+++ src/model/sala/Sala.java
@@ -5,8 +5,40 @@ public class Sala {
 	private Long idSala;
 	private Long codigo;
 	private Integer numeroAunos;
+	private String equipamentos; 
 
-	public Sala() {
+	public Sala(Integer numeroAunos, Long codigo) {
+		this.numeroAunos = numeroAunos;
+		this.codigo = codigo;
+	}
 
+	public Sala(Long idSala, Integer numeroAunos, Long codigo) {
+		this.idSala = idSala;
+		this.numeroAunos = numeroAunos;
+		this.codigo = codigo;
+	}
+
+	public void setCodigo(Long codigo){
+		this.codigo = codigo;
+	}
+
+	public void setNumeroAunos(Integer numeroAunos){
+		this.numeroAunos = numeroAunos;
+	}
+
+	public void setEquipamentos(String equipamentos){
+		this.equipamentos = equipamentos;
+	}
+
+	public Long getCodigo(){
+		return this.codigo;
+	}
+
+	public Integer getNumeroAunos(){
+		return this.numeroAunos;
+	}
+
+	public String getEquipamentos(){
+		return this.equipamentos;
 	}
 }
